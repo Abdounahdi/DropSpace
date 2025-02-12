7d738b679b6367d5d736a24cbdd8c9f53844ca50
Abdou <nahdi.abdo.2020@gmail.com>
2025-02-12 11:25:54 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import GlobalStyles from "./Style/GlobalStyles";
+
+function App() {
+  return (
+    <div>
+      <GlobalStyles />
+      <span>hello</span>
+    </div>
+  );
+}
+
+export default App;
